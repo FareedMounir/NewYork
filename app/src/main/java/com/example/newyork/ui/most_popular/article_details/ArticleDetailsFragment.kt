6d1b99320ebbd1f9b2bd8e5de45e3d1ae04998b3
farid_mounir <farid.mo@rowaad.net>
2020-12-03 18:11:22 +0200

    init commit

--- app/src/main/java/com/example/newyork/ui/most_popular/article_details/ArticleDetailsFragment.kt
+++ app/src/main/java/com/example/newyork/ui/most_popular/article_details/ArticleDetailsFragment.kt
@@ -3,7 +3,6 @@ package com.example.newyork.ui.most_popular.article_details
 import android.os.Bundle
 import android.view.View
 import com.example.newyork.R
-import com.example.newyork.data.models.MostPopularModel
 import com.example.newyork.databinding.FragmentArticleDetailsBinding
 import com.example.newyork.ui.base.BaseBindingFragment
 import org.koin.androidx.viewmodel.ext.android.getViewModel
@@ -20,17 +19,8 @@ class ArticleDetailsFragment :
     private fun getArticle() {
         arguments?.let {
             val article = ArticleDetailsFragmentArgs.fromBundle(it).mostPopular
-            updateView(article)
+            binding.article = article
         }
-    }
-
-    private fun updateView(article: MostPopularModel) {
-
-        binding.titleTv.text = article.title
-        binding.abstractTv.text = article.abstract
-        binding.authorTv.text = article.byline
-        binding.publishedTv.text = "${getString(R.string.published)} ${article.publishedDate}"
-        binding.updatedTv.text = "${getString(R.string.updated)} ${article.updated}"
     }
 
     override fun initViewModel() {
